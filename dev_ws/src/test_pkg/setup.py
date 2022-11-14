5147fb72a064967fd0994c18d2a8ff1ead6cf81f
Ale-Burzio <doctorale99@gmail.com>
2022-11-14 13:01:39 +0100

    container setup

---
+++ dev_ws/src/test_pkg/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'test_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ros',
+    maintainer_email='doctorale99@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'pub_test = test_pkg.pub_test:main'
+        ],
+    },
+)
